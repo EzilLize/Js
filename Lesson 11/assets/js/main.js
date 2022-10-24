30a810ec46b20a1fa7efdfd2e8620ebe8ef1b759
EzilLize <115159321+EzilLize@users.noreply.github.com>
2022-10-24 13:49:28 +0300

    Add files via upload

---
+++ Lesson 11/assets/js/main.js
@@ -0,0 +1,138 @@
+//alert("я javascript!");
+//console.log("Я Java");
+
+//let name=prompt("Ваше имя","Artyom");
+//alert(`Ваше имя ${name}`);
+
+//let a=1 b=1;
+//let c= ++ a;
+//let d= b++;
+//console.log(a)
+//console.log(b)
+//console.log(c)
+//console.log(d)
+
+//let a=2;
+//let x=1 + (a*=2);
+//console.log(a);
+//console.log(x);
+
+//let i1="" +1+0;
+//let i2="" -1+0;
+//let i3=true + false;
+//let i4=6 / "3";
+//let i5="2" * "3";
+//let i6=4+5+"px";
+//let i7="$" +4+5;
+//let i8="4" -2;
+//let i9="4px" -2;
+//let i10="  -9  " +5;
+//let i11="  -9  " -5;
+//let i12=null + 1;
+//let i13=undefined +1;
+//let i14="  \t \n" -2;
+//console.log(i1)
+//console.log(i2)
+//console.log(i3)
+//console.log(i4)
+//console.log(i5)
+//console.log(i6)
+//console.log(i7)
+//console.log(i8)
+//console.log(i9)
+//console.log(i10)
+//console.log(i11)
+//console.log(i12)
+//console.log(i13)
+//console.log(i14)
+
+//let a = +prompt("Первое число?", 1);
+//let b = +prompt("Второе число?", 2);
+//alert(a + b); 
+
+//num=3
+//alert(num)
+
+//a=10;
+//b=2;
+//sum=a+b;
+//raz=a-b;
+//pro=a*b;
+//del=a/b;
+//alert(sum);
+//alert(raz);
+//alert(pro);
+//alert(del);
+
+//c=15;
+//d=2;
+//z=c+d;
+//result=z;
+//alert(result);
+
+//a=10;
+//b=2;
+//c=5;
+//sum=a+b+c;
+//alert(sum);
+
+//a=17;
+//b=10;
+//c=a-b;
+//d=7;
+//result=c+d;
+//alert(result);
+
+//str='Hello, World!';
+//alert(str);
+
+//str1='Hello,';
+//str2=' World!';
+//alert(str1 +str2);
+
+//let name=prompt("Привет,");
+//alert(`Привет, ${name}!`);
+
+//let age=prompt("ваш возраст");
+//alert(`Mне ${age} лет!`);
+
+//let name=prompt("Имя пользователя:");
+//alert(`Ваше имя ${name}`);
+
+//let number=prompt("Введите число:")
+//alert(`Число в квадрате ${number**2}`)
+
+//str="abcde"
+//alert(str[0]);
+//alert(str[1]);
+//alert(str[4]);
+
+//let num="12345";
+//alert(num[0]*num[1]*num[2]*num[3]*num[4])
+
+//alert(60*60);
+//alert(60*60*24);
+//alert(60*60*24*30);
+
+//hour=13;
+//minute=40;
+//second=54;
+//alert(hour);
+//alert(minute);
+//alert(second);
+
+//num=10
+//alert(`${num**2}`)
+
+//var num= 47;
+//num=num +=7;
+//num=num-=18;
+//num=num*=10;
+//num=num /=15;
+//alert(num)
+
+//var num=10;
+//num=num++;
+//num=num++;
+//num=num--;
+//alert(num);
